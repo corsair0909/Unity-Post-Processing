a4626c55d9ba799dbeb0c15240e9cbceb51d32fa
corsair0909 <870233316@qq.com>
2022-06-06 09:57:47 +0800

    实现基于深度图的全局雾效果

--- Assets/Scripts/Fog/GlobalFog.cs
+++ Assets/Scripts/Fog/GlobalFog.cs
@@ -1,18 +1,127 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class GlobalFog : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Shader shader;
+    
+    private Material _material;
+    public Material Mate
     {
-        
+        get
+        {
+            if (_material==null)
+            {
+                _material = new Material(shader);
+                return _material;
+            }
+            else
+            {
+                return _material;
+            }
+        }
+    }
+    
+    private Camera _camera;
+    public Camera Cam
+    {
+        get
+        {
+            if (_camera==null)
+            {
+                _camera = GetComponent<Camera>();
+                return _camera;
+            }
+            else
+            {
+                return _camera;
+            }
+        }
+    }
+    
+    private Transform _transform;
+    public Transform CamTrans
+    {
+        get
+        {
+            if (_transform==null)
+            {
+                _transform = Cam.transform;
+                return _transform;
+            }
+            else
+            {
+                return _transform;
+            }
+        }
+    }
+    
+    [Range(0.0f,3.0f)]
+    public float fogDensity = 0;
+    public Color fogColor = Color.white;
+    [Range(0,5)]
+    public float fogStart = 0;
+    [Range(0,5)]
+    public float fogEnd = 2;
+
+    private void OnEnable()
+    {
+        Cam.depthTextureMode  |= DepthTextureMode.DepthNormals;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
+        if (Mate)
+        {
+            Matrix4x4 furstum = Matrix4x4.identity;
+            float fov = Cam.fieldOfView;
+            float aspect = Cam.aspect;
+            float near = Cam.nearClipPlane;
+            float far = Cam.farClipPlane;
+
+            float halfHeight = near * Mathf.Tan(fov * 0.5f * Mathf.Deg2Rad);
+            Vector3 toTop = halfHeight * CamTrans.up;
+            Vector3 toRight = halfHeight * CamTrans.right * aspect;
+
+            Vector3 LT = CamTrans.forward * near + toTop - toRight;
+            //采样四个顶点的深度得到的并非顶点到相机的欧式距离，而是在Z轴方向上的距离
+            //深度/距离（顶点到相机距离） = 近裁剪平面距离/距离的模长（顶点到相机距离）
+            float scale = LT.magnitude / near;
+            LT.Normalize();
+            LT *= scale ;
         
+            Vector3 RT = CamTrans.forward * near + toTop + toRight;
+            RT.Normalize();
+            RT *= scale;
+        
+            Vector3 LB = CamTrans.forward * near - toTop - toRight;
+            LB.Normalize();
+            LB *= scale;
+        
+            Vector3 RB = CamTrans.forward * near - toTop + toRight;
+            RB.Normalize();
+            RB *= scale;
+        
+            furstum.SetRow(0,LT);
+            furstum.SetRow(1,RT);
+            furstum.SetRow(2,LB);
+            furstum.SetRow(3,RB);
+        
+            Mate.SetMatrix("_furstum",furstum);
+            Mate.SetMatrix("ViewPrjiectionInverseMatrix",(Cam.worldToCameraMatrix*Cam.projectionMatrix).inverse);
+            Mate.SetFloat("_Density",fogDensity);
+            Mate.SetColor("_fogColor",fogColor);
+            Mate.SetFloat("_fogStart",fogStart);
+            Mate.SetFloat("_forEnd",fogEnd);
+        
+            Graphics.Blit(src,dest,Mate);
+        }
+        else
+        {
+            Graphics.Blit(src,dest);
+        }
     }
 }
--- Assets/Shader/GlobalFog.shader
+++ Assets/Shader/GlobalFog.shader
@@ -6,51 +6,73 @@ Shader "Unlit/GlobalFog"
     }
     SubShader
     {
-        Tags { "RenderType"="Opaque" }
-        LOD 100
-
         Pass
         {
+            ZTest Always
+            ZWrite Off
+            Cull Off
+            
             CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
-            // make fog work
-            #pragma multi_compile_fog
-
             #include "UnityCG.cginc"
-
-            struct appdata
-            {
-                float4 vertex : POSITION;
-                float2 uv : TEXCOORD0;
-            };
 
             struct v2f
             {
                 float2 uv : TEXCOORD0;
-                UNITY_FOG_COORDS(1)
+                float2 depthUV : TEXCOORD1;
                 float4 vertex : SV_POSITION;
+                float4 Ray : TEXCOORD2;
             };
 
             sampler2D _MainTex;
-            float4 _MainTex_ST;
+            sampler2D _CameraDepthTexture;
+            half4 _fogColor;
+            fixed _fogStart;
+            fixed _forEnd;
+            fixed _Density;
+            float4x4 _furstum;
+            float4 _MainTex_TexelSize;
 
-            v2f vert (appdata v)
+            v2f vert (appdata_img v)
             {
                 v2f o;
                 o.vertex = UnityObjectToClipPos(v.vertex);
-                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
-                UNITY_TRANSFER_FOG(o,o.vertex);
+                o.uv = v.texcoord;
+                o.depthUV = v.texcoord;
+                
+                fixed index = 0;
+                if (v.texcoord.x < 0 && v.texcoord.y > 0)
+                {
+                    index = 0;
+                }
+                else if(v.texcoord.x > 0 && v.texcoord.y > 0)
+                {
+                    index = 1;
+                }
+                else if(v.texcoord.x < 0 && v.texcoord.y < 0)
+                {
+                    index = 2;
+                }
+                else if (v.texcoord.x > 0 && v.texcoord.y < 0)
+                {
+                    index = 3;
+                }
+                o.Ray = _furstum[index];
                 return o;
             }
 
             fixed4 frag (v2f i) : SV_Target
             {
-                // sample the texture
+                float3 linearDepth = LinearEyeDepth(SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture,i.depthUV));
+                float3 worldPos = _WorldSpaceCameraPos + linearDepth * i.Ray.xyz;
+
+                float fogDensity = (_forEnd - worldPos.y)/(_forEnd - _fogStart);
+                fogDensity = saturate(fogDensity * _Density);
+                
                 fixed4 col = tex2D(_MainTex, i.uv);
-                // apply fog
-                UNITY_APPLY_FOG(i.fogCoord, col);
-                return col;
+                fixed4 finalColor = lerp(col,_fogColor,fogDensity);
+                return finalColor;
             }
             ENDCG
         }
